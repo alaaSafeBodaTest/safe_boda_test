4900236ed9aa031a39ac60e12a31379b769cbb4c
Alaa <alaa.ibrahim.it.97@gmail.com>
2022-09-27 10:21:47 +0300

    connect_project_to_firebase

---
+++ app/src/main/java/com/example/safebodatest/features/login/presentation/view/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.safebodatest.features.login.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.safebodatest.R
+import com.example.safebodatest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
